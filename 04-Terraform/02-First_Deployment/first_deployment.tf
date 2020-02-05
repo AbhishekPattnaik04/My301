f54ec1a9e9e6f8b15c836d05850b4fc781c499e0
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-02-05 15:05:51 +0530

    Update first_deployment.tf

--- 04-Terraform/02-First_Deployment/first_deployment.tf
+++ 04-Terraform/02-First_Deployment/first_deployment.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   access_key = "AKIAYKBCL35Z4D6JMLFS"
   secret_key = "AhZkSLcIRxe7i2PMEzHReWWMhCyciG1QD0E3sUzM"
-  region     = "us-west-2"
+  region     = "us-east-2"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0994c095691a46fb5"
+  ami           = "ami-0d03add87774b12c5"
   instance_type = "t2.micro"
 }
